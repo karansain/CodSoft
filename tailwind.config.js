b1ec63a6bbf642e786892fb54caa2c06780d8ab2
karansain <karansainnn@gmail.com>
2024-07-05 00:45:47 +0530

    Final Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            width: {
+                '550': '550px',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
